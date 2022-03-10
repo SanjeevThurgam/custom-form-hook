42628f52713e6ffbf1a293467dc4f480d4ac5d61
william-b-henderson <william_henderson@berkeley.edu>
2022-03-10 13:03:45 -0800

    Start to implement useForm

--- src/Hooks/useForm.tsx
+++ src/Hooks/useForm.tsx
@@ -1,21 +1,43 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 
 // TODO: Define necessary typescript prop types, other types.
 
 const useForm = () => {
   // TODO: Define necessary state variables and helper functions.
-  const handleSubmit = () => {
-    return 1; // TODO
+
+  // const [inputs, setInputs] = useState({});
+  // const [firstName, setFirstName] = useState();
+  // const [isInvalid, setIsInvalid] = useState(true);
+
+  // const onChange = (e: unknown) => {
+  //   setInputs(e.target.value);
+  //   setIsValid(checkIfValid());
+  // }
+
+  // const checkIfValid = () => {
+  //   return true
+  // }
+
+
+  const handleSubmit = (onSubmit: FormEventHandler<HTMLDivElement>) => {
+
+    //Check that the input is proper with the registers, if it is proper, return onSubmit, else return a diff function that alerts with highlights
+
+
+    return onSubmit; 
   };
 
-  const register = () => {
-    return 1; // TODO
-  };
+  // const register = (name: String) => {
+  //   setInputs(inputs)
+    
+
+  //   return {onChange, isInvalid}; // TODO
+  // };
 
   return {
     handleSubmit,
-    register,
+    // register,
   };
 };
 
--- src/PageComponents/ContentContainer.tsx
+++ src/PageComponents/ContentContainer.tsx
@@ -3,7 +3,7 @@ import {
   Flex,
   FormControl,
   Switch,
-  Input,
+//   Input,
   InputGroup,
   InputLeftAddon,
   FormLabel,
@@ -11,46 +11,62 @@ import {
   Button,
   Stack
 } from "@chakra-ui/react";
+// import useForm from '../Hooks/useForm'
 
-export const ContentContainer = () => (
-<Flex direction='column' w='50%' h='50%' alignItems='flex-start' textAlign='left' justify='space-between'>
-    <FormControl alignContent='space-between'>
-        <InputGroup margin='10px'>
-            <InputLeftAddon>
-                Name
-            </InputLeftAddon>
-            <Input type='text' placeholder='Chad Chaddington'/>
-        </InputGroup>
-        <Stack justify='start' align='start' direction='row' margin='10px'>
-            <FormLabel htmlFor='Frontend'>
-                I want to learn frontend
-            </FormLabel>
-            <Switch id='Frontend'/>
-        </Stack>
-        <Stack justify='start' align='start' direction='row' margin='10px'>
-            <FormLabel htmlFor='Backend'>
-                I want to learn backend
-            </FormLabel>
-            <Switch id='Backend'/>
-        </Stack>
-        <Select placeholder='I am a...' margin='10px'>
-            <option value='freshman'>
-                Freshman
-            </option>
-            <option value='sophomore'>
-                Sophomore
-            </option>
-            <option value='junior'>
-                Junior
-            </option>
-            <option value='senior'>
-                Senior
-            </option>
-        </Select>
-        <Button type='submit' colorScheme='blue' margin='10px'>
-            Submit
-        </Button>
-    </FormControl>
-</Flex>
-);
+let counter = 0;
+
+export const ContentContainer = () => {
+    
+
+    // const { register, handleSubmit } = useForm();
+    // register("hi");
+    
+    const onSubmit = (e: object) => {
+        console.log(e)
+    }
+
+
+    return (
+        <Flex direction='column' w='50%' h='50%' alignItems='flex-start' textAlign='left' justify='space-between'>
+            <FormControl alignContent='space-between' /*onSubmit={handleSubmit(onSubmit)} */>
+                <InputGroup margin='10px'>
+                    <InputLeftAddon>
+                        Name
+                    </InputLeftAddon>
+                    {/* <Input type='text' placeholder='Chad Chaddington' {...register("Name")}/> */}
+                </InputGroup>
+                <Stack justify='start' align='start' direction='row' margin='10px'>
+                    <FormLabel htmlFor='Frontend'>
+                        I want to learn frontend
+                    </FormLabel>
+                    <Switch id='Frontend'/>
+                </Stack>
+                <Stack justify='start' align='start' direction='row' margin='10px'>
+                    <FormLabel htmlFor='Backend'>
+                        I want to learn backend
+                    </FormLabel>
+                    <Switch id='Backend'/>
+                </Stack>
+                <Select placeholder='I am a...' margin='10px'>
+                    <option value='freshman'>
+                        Freshman
+                    </option>
+                    <option value='sophomore'>
+                        Sophomore
+                    </option>
+                    <option value='junior'>
+                        Junior
+                    </option>
+                    <option value='senior'>
+                        Senior
+                    </option>
+                </Select>
+                <p>Render: {counter++}</p>
+                <Button type='submit' colorScheme='blue' margin='10px' onClick={onSubmit}>
+                    Submit
+                </Button>
+            </FormControl>
+        </Flex>
+    )
+};
 export default ContentContainer; 
